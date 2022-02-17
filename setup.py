096ab951a82e2b61a43b8e72bad1fde09aff42f1
MyungHoonJin <jinmang2@gmail.com>
2022-02-18 03:10:37 +0900

    fix repo name in setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup, find_packages
 VERSION = {}  # type: ignore
 with open("dooly/__version__.py", "r") as version_file:
     exec(version_file.read(), VERSION)
-    
 
-setup(name='dooly', 
+
+setup(name='dooly',
       version=VERSION["version"],
-      url='https://github.com/jinmang2/dooly', 
-      author='jinmang2', 
-      author_email='jinmang2@gmail.com', 
-      description='A library that handles everything with 🤗 and supports batching to models in PORORO', 
-      packages=find_packages(exclude=['tests']), 
-      long_description=open('README.md').read(), 
-      long_description_content_type='text/markdown', 
+      url='https://github.com/jinmang2/DOOLY', 
+      author='jinmang2',
+      author_email='jinmang2@gmail.com',
+      description='A library that handles everything with 🤗 and supports batching to models in PORORO',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       install_requires=['torch'],
       zip_safe=False,
       classifiers=[
