baeba27bf580cb7c4da6d15e81a800ab20b4d57a
MyungHoonJin <jinmang2@gmail.com>
2022-02-18 03:09:54 +0900

    delete unused files

--- dooly/tokenization_base.py
+++
@@ -1,9 +0,0 @@
-from .utils.file_utils import get_list_of_files, hf_bucket_url, cached_path
-from .constant import HUB_PATH_OR_URL
-
-
-class DoolyTokenizerBase:
-    
-    @classmethod
-    def from_pretrained(cls):
-        pass
