376f559276c6e0bc8a0afe0a6af4a3ab06aee526
MyungHoonJin <jinmang2@gmail.com>
2022-03-05 23:50:29 +0900

    fix bug on zsl

--- dooly/tasks/zero_shot_classification.py
+++ dooly/tasks/zero_shot_classification.py
@@ -68,7 +68,7 @@ class ZeroShotClassification(NaturalLanguageInference):
         label2id = self._model.config.label2id
         contradiction_id = label2id.get(self.contra_label_name, None)
         entailment_id = label2id.get(self.entail_label_name, None)
-        assert contradiction_id is None or entailment_id is None
+        assert contradiction_id is not None or entailment_id is not None
         return [contradiction_id, entailment_id]
 
     def __call__(
