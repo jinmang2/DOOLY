6bf2df1cf4a86638fdb52d7c43a40be8f072d7ea
MyungHoonJin <jinmang2@gmail.com>
2022-06-16 09:29:44 +0900

    #21 refactor dooly tokenizer load method

--- dooly/tokenizers/__init__.py
+++ dooly/tokenizers/__init__.py
@@ -1,6 +1,6 @@
-import json
-from functools import partial
-from typing import Union, Optional
+from typing import Type
+
+from transformers import PreTrainedTokenizer
 
 from .base import Tokenizer
 from .bpe import Gpt2BpeTokenizer, BpeJaZhTokenizer
@@ -13,12 +13,7 @@ from .hf_tokenizer import (
 from .pos_tagger import (
     PosDpTokenizer,
 )
-from ..build_utils import (
-    download_from_hf_hub,
-    HUB_NAME,
-    VOCAB_NAME,
-    TOKENIZER_USER_AGENT,
-)
+from ..utils import DOOLY_HUB_NAME
 
 
 DoolyTokenizerHub = {
@@ -55,107 +50,90 @@ DoolyTokenizerHub = {
 }
 DoolyTokenizerHub["bt"] = DoolyTokenizerHub["mt"]
 DoolyTokenizerHub["zero_topic"] = DoolyTokenizerHub["nli"]
+
 available_tasks = list(DoolyTokenizerHub.keys())
 
 
-class DoolyTokenizer:
-    """ Dooly Tokenizer """
+def load_pretrained_tokenizer(
+    pretrained_model_name_or_path: str,
+    tokenizer_class: Type[PreTrainedTokenizer],
+    **kwargs,
+) -> PreTrainedTokenizer:
+    return tokenizer_class.from_pretrained(
+        pretrained_model_name_or_path, **kwargs
+    )
 
-    @classmethod
-    def build_tokenizer(
-        cls, task: str, lang: str, n_model: Optional[str] = None, **kwargs
+
+def load_tokenizer_from_dooly_hub(
+    subfolder: str,
+    tokenizer_class: Type[PreTrainedTokenizer],
+    **kwargs,
+) -> PreTrainedTokenizer:
+
+    def _load_pretrained(
+        pretrained_model_name_or_path: str, subfolder: str, **kwargs
     ):
-        assert (
-            task in available_tasks
-        ), f"Task `{task}` is not available. See here {available_tasks}."
-        available_langs = DoolyTokenizerHub[task]
-        assert lang in available_langs, (
-            f"Language `{lang}` is not available in this task {task}. "
-            f"See here {available_langs}."
-        )
-        available_models = available_langs[lang]
-        if n_model is None:
-            n_model = list(available_models.keys())[0]
-        assert n_model in available_models, (
-            f"Model `{n_model}` is not available in this task-lang pair. "
-            f"See here {available_models}."
+        return tokenizer_class.from_pretrained(
+            pretrained_model_name_or_path, subfolder=subfolder, **kwargs
         )
 
+    return _load_pretrained(
+        pretrained_model_name_or_path=DOOLY_HUB_NAME,
+        subfolder=subfolder, **kwargs
+    )
+
+
+def load_dooly_tokenizer(
+    pretrained_model_name_or_path: str = None,
+    tokenizer_class: Type[PreTrainedTokenizer] = None,
+    task: str = None,
+    lang: str = None,
+    n_model: str = None,
+    **kwargs,
+) -> PreTrainedTokenizer:
+    if pretrained_model_name_or_path is not None:
+        if tokenizer_class is None:
+            raise ValueError(
+                "If you are using the personal huggingface.co model, "
+                "`tokenizer_class` parameter is required."
+            )
+        return load_pretrained_tokenizer(
+            pretrained_model_name_or_path, tokenizer_class
+        )
+
+    if all([task is None and lang is None and n_model is None]):
+        raise ValueError(
+            "`task`, `lang`, and `n_model` parameters are required to "
+            "access the subfolder of dooly-hub.\nCheck your parameters! "
+            f"`task`: {task} `lang`: {lang} `n_model`: {n_model}."
+        )
+
+    assert (
+        task in available_tasks
+    ), f"Task `{task}` is not available. See here {available_tasks}."
+
+    available_langs = DoolyTokenizerHub[task]
+    assert lang in available_langs, (
+        f"Language `{lang}` is not available in this task {task}. "
+        f"See here {available_langs}."
+    )
+
+    available_models = available_langs[lang]
+    assert n_model in available_models, (
+        f"Model `{n_model}` is not available in this task-lang pair. "
+        f"See here {available_models}."
+    )
+
+    subfolder = f"{task}/{lang}/{n_model}"
+    subfolder_postfix = kwargs.pop("subfolder_postfix", None)
+    if subfolder_postfix is not None:
+        subfolder += f"/{subfolder_postfix}"
+
+    if tokenizer_class is None:
         tokenizer_class = available_models[n_model]
 
-        return cls._build_tokenizer(task, lang, n_model, tokenizer_class, **kwargs)
+    kwargs.update({"task": task, "lang": lang, "n_model": n_model})
 
-    @classmethod
-    def _build_tokenizer(
-        cls,
-        task: str,
-        lang: str,
-        n_model: str,
-        tokenizer_class: Union[Tokenizer, PreTrainedTokenizerBase],
-        revision: Optional[str] = None,
-        cache_dir: Optional[str] = None,
-        force_download: bool = False,
-        resume_download: bool = False,
-        **kwargs,
-    ) -> Union[Tokenizer, PreTrainedTokenizerBase]:
-
-        if issubclass(tokenizer_class, PreTrainedTokenizerBase):
-            tokenizer = tokenizer_class.from_pretrained(
-                pretrained_model_name_or_path=HUB_NAME,
-                subfolder=f"{task}/{lang}/{n_model}",
-                revision=revision,
-                cache_dir=cache_dir,
-                force_download=force_download,
-                resume_download=resume_download,
-                **kwargs,
-            )
-            tokenizer.lang = lang
-            return tokenizer
-
-        _download_from_hf_hub = partial(
-            download_from_hf_hub,
-            model_id=HUB_NAME,
-            subfolder=f"{task}/{lang}/{n_model}",
-            revision=revision,
-            cache_dir=cache_dir,
-            force_download=force_download,
-            resume_download=resume_download,
-            user_agent=TOKENIZER_USER_AGENT,
-        )
-
-        # Load from URL or cache if already cached
-        resolved_vocab_file = _download_from_hf_hub(filename=VOCAB_NAME)
-
-        # _dict_from_json_file
-        with open(resolved_vocab_file, "r", encoding="utf-8") as f:
-            vocab = json.load(f)
-
-        if "pos" in tokenizer_class.__name__.lower():
-            resolved_pos_vocab_file = _download_from_hf_hub(filename="pos_vocab.json")
-
-            with open(resolved_pos_vocab_file, "r", encoding="utf-8") as f:
-                pos_vocab = json.load(f)
-
-            kwargs.update({"pos_vocab": pos_vocab})
-
-        tokenizer = tokenizer_class(lang, vocab, **kwargs)
-
-        if "bpe" in tokenizer_class.__name__.lower():
-            encoder = None
-            bpe_merges = None
-
-            if lang == "en":
-                encoder_json = _download_from_hf_hub(filename="encoder.json")
-                with open(encoder_json, "r") as f:
-                    encoder = json.load(f)
-
-                vocab_bpe = _download_from_hf_hub(filename="vocab.bpe")
-                with open(vocab_bpe, "r", encoding="utf-8") as f:
-                    bpe_data = f.read()
-                bpe_merges = [
-                    tuple(merge_str.split()) for merge_str in bpe_data.split("\n")[1:-1]
-                ]
-
-            tokenizer._build_bpe(lang, encoder, bpe_merges)
-
-        return tokenizer
+    return load_tokenizer_from_dooly_hub(
+        subfolder=subfolder, tokenizer_class=tokenizer_class, **kwargs
+    )
